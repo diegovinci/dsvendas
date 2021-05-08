11bd265e50a251e0ea029538a5d6da3af3cdd759
diegovinci <diego.vinci@outlook.com>
2021-05-08 09:41:12 -0300

    feat: add pageable sales

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
@@ -0,0 +1,87 @@
+package com.devsuperior.dsvendas.dto;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import com.devsuperior.dsvendas.entities.Sale;
+
+public class SaleDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private Integer visited;
+	private Integer deals;
+	private Double amounts;
+	private LocalDate date;
+
+	private SellerDTO seller;
+
+	public SaleDTO() {}
+
+	public SaleDTO(Long id, Integer visited, Integer deals, Double amounts, LocalDate date, SellerDTO seller) {
+		this.id = id;
+		this.visited = visited;
+		this.deals = deals;
+		this.amounts = amounts;
+		this.date = date;
+		this.seller = seller;
+	}
+
+	public SaleDTO(Sale entity) {
+		id = entity.getId();
+		visited = entity.getVisited();
+		deals = entity.getDeals();
+		amounts = entity.getAmount();
+		date = entity.getDate();
+		seller = new SellerDTO(entity.getSeller());
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public Double getAmounts() {
+		return amounts;
+	}
+
+	public void setAmounts(Double amounts) {
+		this.amounts = amounts;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public SellerDTO getSeller() {
+		return this.seller;
+	}
+
+	public void setSeller(SellerDTO seller) {
+		this.seller = seller;
+	}
+}
--- backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 import com.devsuperior.dsvendas.entities.Seller;
 
-public class SellerDTO implements Serializable{
+public class SellerDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
