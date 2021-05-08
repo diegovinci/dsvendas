75becc0759ebfe713486098c8db51ca7d88aa8c1
diegovinci <diego.vinci@outlook.com>
2021-05-08 20:45:35 -0300

    feat:  add BarChart integration using react hooks

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,30 +1,68 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 import Chart from "react-apexcharts";
+import { saleSuccess } from "types/sale";
+import { round } from "utils/format";
+import { BASE_URL } from "utils/requests";
+
+type SeriesData = {
+  name: string;
+  data: number[];
+}
+
+type ChartData = {
+  labels: {
+    categories: string[]
+  };
+  series: SeriesData[];
+}
 
 const BarChart = () => {
-  const options = {
-    plotOptions: {
-        bar: {
-            horizontal: true,
-        }
-    },
-  };
-  
-  const mockData = {
+  const [chartData, setChartData] = useState<ChartData>({
     labels: {
-        categories: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"]
+      categories: []
     },
     series: [
-        {
-            name: "% Sucesso",
-            data: [43.6, 67.1, 67.7, 45.6, 71.1]                   
-        }
+      {
+        name: "% Sucesso",
+        data: []
+      }
     ]
+  });
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/success-by-seller`).then(response => {
+      const data = response.data as saleSuccess[];
+      const labels = data.map(item => item.sellerName);
+      const series = data.map(item => round(100 * item.deals / item.visited, 1));
+
+      setChartData({
+        labels: {
+          categories: labels
+        },
+        series: [
+          {
+            name: "% Sucesso",
+            data: series
+          }
+        ]
+      });
+    });
+  }, []);
+
+  const options = {
+    plotOptions: {
+      bar: {
+        horizontal: true,
+      }
+    },
   };
 
   return (
-    <Chart 
-      options={{ ...options, xaxis: mockData.labels }}
-      series={mockData.series}
+    <Chart
+      options={{ ...options, xaxis: chartData.labels }}
+      series={chartData.series}
       type="bar"
       height="240"
     />
@@ -32,3 +70,5 @@ const BarChart = () => {
 }
 
 export default BarChart;
+
+
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -23,16 +23,12 @@ const DonutChart = () => {
     });
   }, []);
 
-  // const mockData = {
-  //   series: [477138, 499928, 444867, 220426, 473088],
-  //   labels: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"]
-  // }
-
   const options = {
     legend: {
       show: true
     }
   }
+
   return (
     <Chart
       options={{ ...options, labels: chartData.labels }}
