1b999e96a33042c5e1b29e1846a21612fa77d432
diegovinci <diego.vinci@outlook.com>
2021-05-08 22:41:35 -0300

    refactor: replace single quote for double quote

--- frontend/src/components/Paginatin/index.tsx
+++ frontend/src/components/Paginatin/index.tsx
@@ -10,13 +10,13 @@ const Pagination = ({ page, onPageChange }: Props) => {
     <div className="row d-flex justify-content-center">
       <nav>
         <ul className="pagination justify-content-center">
-          <li className={`page-item ${page.first ? 'disabled' : ''}`}>
+          <li className={`page-item ${page.first ? "disabled" : ""}`}>
             <button className="page-link" onClick={() => onPageChange(page.number - 1)}>Anterior</button>
           </li>
           <li className="page-item disabled">
             <span className="page-link">{page.number + 1}</span>
           </li>
-          <li className={`page-item ${page.last ? 'disabled' : ''}`}>
+          <li className={`page-item ${page.last ? "disabled" : ""}`}>
             <button className="page-link" onClick={() => onPageChange(page.number + 1)}>Próxima</button>
           </li>
         </ul>
